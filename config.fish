7382a74b0a6564979f43fda92b7de2b1bc80b9b6
LambLeeg <59935189+LambLeeg@users.noreply.github.com>
2022-11-08 19:03:49 +0300

    Update config.fish

--- config.fish
+++ config.fish
@@ -4,11 +4,11 @@ set TERM "xterm-256color"
 
 
 ### "bat" as manpager
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANPAGER "sh -c 'col -bx | batcat -l man -p'"
 
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
+alias exa='exa -al --color=always --group-directories-first' # my preferred listing
 alias la='exa -a --color=always --group-directories-first'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first'  # long format
 alias lt='exa -aT --color=always --group-directories-first' # tree listing
@@ -19,6 +19,11 @@ alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
+#Apt-fast
+alias af='apt-fast'
+
+#Intel
+alias intel='intel_gpu_top'
 
 # switch between shells
 alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
@@ -30,3 +35,4 @@ alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
 starship init fish | source
 
 set -U fish_user_paths /home/iam/.cargo/bin $fish_user_paths
+
